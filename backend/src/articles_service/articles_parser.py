037c2aae37a08391ec86ab41cb2891a3e5cc1ba8
Marek Hajduk <m.hajduk@nomtek.com>
2023-10-23 09:42:06 +0200

    small cleanup of the code

--- backend/src/articles_service/articles_parser.py
+++ backend/src/articles_service/articles_parser.py
@@ -11,52 +11,41 @@ def format_text(text, leave_paragraphs=True):
     return text
 
 
-class PDFparser:
-
+class ArticleParser:
     def __init__(self, pdf_name):
         self.filename = pdf_name
+        self.text = extract_text(self.filename, maxpages=2)
 
-    def get_text(self):
-        text = extract_text(self.filename)
-        return format_text(text)
-
-    def get_text_raw(self):
-        return extract_text(self.filename, maxpages=1)
-
-    # gets the article abstract
-    def get_abstract(self):
-        text = extract_text(self.filename, maxpages=2)
-        abstract = re.findall("(?i)abstract(?:.|\n)(?:.+\n)+", text)
+    def get_abstract(self) -> str:
+        abstract = re.findall("(?i)abstract((?:.|\n)(?:.+\n)+)", self.text)
         if len(abstract) > 0:
             return format_text(abstract[0], False)
-        return ""
+        raise ValueError("couldn't find the abstract")
 
-    # gets the article keywords
-    def get_keywords(self):
-        text = extract_text(self.filename, maxpages=2)
-        keywords = re.findall("(?i)keywords:((?:.|\n)(?:.+\n)+)", text)
+    def get_keywords(self) -> list[str]:
+        keywords = re.findall("(?i)keywords:((?:.|\n)(?:.+\n)+)", self.text)
         if len(keywords) > 0:
             keywords[0] = keywords[0].strip()
             keywords = keywords[0].split(", ")
             return keywords
-        return ""
+        raise ValueError("couldn't find the keywords")
 
-    def get_emails(self):
-        text = self.get_text()
-        emails = re.findall('[a-zA-Z]\S+@\S+[a-zA-Z]', text)
+    def get_emails(self) -> list[str]:
+        emails = re.findall("[a-zA-Z]\S+@\S+[a-zA-Z]", self.text)
         if len(emails) > 0:
             return emails
         return ""
 
     # from file name?
-    def get_title_filename(self):
-        title = self.filename.split('_')
+    def get_title_filename(self) -> str:
+        title = self.filename.split("_")
         title = title[1].split(".")
         return title[0]
 
-    def get_authors(self):
-        authors = extract_text(self.filename, maxpages=1)
-        authors = re.findall("(?i)complete list of authors:(?:.|\n)*keywords", authors)
+    def get_authors(self) -> str:
+        authors = re.findall(
+            "(?i)complete list of authors:((?:.|\n)*)keywords", self.text
+        )
         if len(authors) > 0:
             return format_text(authors[0], False)
         return ""

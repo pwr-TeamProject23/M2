aa18f9dec0012e9bdc47b634ce79f596443d5e9a
Marek Hajduk <m.hajduk@nomtek.com>
2023-10-30 11:54:05 +0100

    removed unused imports

--- backend/src/tests/article_parser_tests/test_article_parser.py
+++ backend/src/tests/article_parser_tests/test_article_parser.py
@@ -1,11 +1,3 @@
-import pytest
-from backend.src.articles_service import articles_parser
-import os
-
-
-# pytestmark = pytest.mark.parametrize("article", os.listdir("articles"), indirect=True)
-
-
 def test_get_keywords(article):
     keywords = article.get_keywords()
     assert len(keywords) >= 1

06d016a149e1ac4f75c01e1ea4c5b96d2ffb8679
SzymonKieczkaStudent <szymon-kieczka@o2.pl>
2023-10-30 12:54:59 +0100

    FastAPI skeleton for connection with front-end. Uploading pdf files.

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,8 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, HTTPException
+from typing import Annotated
+import os
 
-app = FastAPI(title="FastAPI")
+app = FastAPI(title="ZRECENZOWANE")
 
 
 @app.get("/")
-def read_root():
-    return {"hello": "world"}
+async def root():
+    return {"greeting": "hello"}
+
+
+@app.post("/upload/file/", status_code=200)
+async def upload_pdf(file: UploadFile):
+    if file.content_type != "application/pdf":
+        raise HTTPException(400, detail="Invalid document type.")
+    try:
+        file_content = file.file
+    except Exception:
+        raise HTTPException(500, detail="Internal server error.")
+    return {"message": "successful upload", "filename": file.filename}
+
+
+@app.get("/upload/results/", status_code=200)
+async def retrieve_results():
+    pass
+
+
+@app.get("user_history/")
+async def user_history():
+    pass
+

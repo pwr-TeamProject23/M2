f7fb09ecbab6f91cbb46870a86440a512409aaa0
biegasj <biegasj@gmail.com>
2023-11-16 10:46:27 +0100

    Add Forbidden exception and refactor exception names

--- backend/src/common/exceptions.py
+++ backend/src/common/exceptions.py
@@ -1,18 +1,27 @@
-from typing import Any
+from http import HTTPStatus
+
 from fastapi import HTTPException
 
 
-class NotFound(HTTPException):
+class NotFoundException(HTTPException):
     def __init__(
         self,
         headers: dict[str, str] | None = None,
     ):
-        super().__init__(404, "Not found", headers)
+        super().__init__(HTTPStatus.NOT_FOUND, "Not found", headers)
 
-    
-class Unauthorized(HTTPException):
+
+class ForbiddenException(HTTPException):
     def __init__(
         self,
         headers: dict[str, str] | None = None,
     ):
-        super().__init__(403, "Unauthorized", headers)
+        super().__init__(HTTPStatus.FORBIDDEN, "Forbidden", headers)
+
+
+class UnauthorizedException(HTTPException):
+    def __init__(
+        self,
+        headers: dict[str, str] | None = None,
+    ):
+        super().__init__(HTTPStatus.UNAUTHORIZED, "Unauthorized", headers)

0dc10748a90cfa9264301d0b6f5a9563066ff938
biegasj <biegasj@gmail.com>
2023-11-19 16:30:30 +0100

    Change imports from relative to absolute in the models module

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import declarative_base
 
 BaseModel = declarative_base()
 
-from .session import UserSession
-from .user import User
-from .upload import Upload
-from .reviewer import Reviewer
+from src.models.reviewer import Reviewer
+from src.models.session import UserSession
+from src.models.upload import Upload
+from src.models.user import User

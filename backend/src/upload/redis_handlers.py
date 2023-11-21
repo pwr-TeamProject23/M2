e6f8751044c09e85e3603ab72d70b789b4f7b315
biegasj <biegasj@gmail.com>
2023-11-21 23:27:42 +0100

    Add Redis handlers

---
+++ backend/src/upload/redis_handlers.py
@@ -0,0 +1,21 @@
+from src.redis_handler import RedisHandler
+from enum import Enum
+
+
+class UploadStatus(str, Enum):
+    PENDING: "PENDING"
+
+
+class UploadStatusRedisHandler:
+    def __init__(self, redis: RedisHandler):
+        self.redis = redis
+
+    def get_status(self, upload_id: int) -> str | None:
+        status_key = f"status:{upload_id}"
+        return self.redis.get(status_key)
+
+    def set_status(
+        self, upload_id: int, status: UploadStatus | str = UploadStatus.PENDING
+    ) -> bool:
+        status_key = f"status:{upload_id}"
+        return self.redis.set(status_key, status)

168d75a8efc14f9877c9fd448693abc1c5dc1841
biegasj <biegasj@gmail.com>
2023-11-22 11:03:40 +0100

    Add Redis abstraction layer

---
+++ backend/src/upload/redis_handlers.py
@@ -0,0 +1,17 @@
+from src.redis_handler import RedisHandler
+from src.upload.status import UploadStatus
+
+
+class UploadStatusRedisHandler:
+    def __init__(self, redis: RedisHandler):
+        self.redis = redis
+
+    def get_status(self, upload_id: int) -> str | None:
+        status_key = f"status:{upload_id}"
+        return self.redis.get(status_key)
+
+    def set_status(
+        self, upload_id: int, status: UploadStatus | str = UploadStatus.PENDING
+    ) -> bool:
+        status_key = f"status:{upload_id}"
+        return self.redis.set(status_key, status)

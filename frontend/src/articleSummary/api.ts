159f755b4e0725a6adda2452b5e5d2130f9fa2bc
olga <olga.szat@gmail.com>
2023-11-26 13:55:18 +0100

    Add mocked endpoint which returns affiliation

--- frontend/src/articleSummary/api.ts
+++ frontend/src/articleSummary/api.ts
@@ -4,6 +4,6 @@ import { SuggestionsReponseModel } from "./models";
 export const getSuggestions = async (
   upload_id: string,
 ): Promise<SuggestionsReponseModel> => {
-  const response = await axios.get(`/upload/results/${upload_id}`);
+  const response = await axios.get(`/upload/${upload_id}/results`);
   return response.data;
 };

20c0f0d31d79f2266a3080a85aabcf5446c66f12
biegasj <biegasj@gmail.com>
2023-11-30 18:13:44 +0100

    Adjust db models to processed data and make primary keys auto-incremental

--- backend/src/models/author.py
+++ backend/src/models/author.py
@@ -14,12 +14,12 @@ class Source(str, Enum):
 class Author(BaseModel):
     __tablename__ = "author"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     search_id: Mapped[int] = mapped_column(ForeignKey("search.id"))
     author_external_id: Mapped[str]
     first_name: Mapped[str]
     last_name: Mapped[str]
-    affiliation: Mapped[str]
+    affiliation: Mapped[str | None]
     email: Mapped[str | None]
     source: Mapped[Source]
     search: Mapped["Search"] = relationship(back_populates="authors")

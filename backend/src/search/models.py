7501e81364c7b471d391681bc84822e52d3fcda7
biegasj <biegasj@gmail.com>
2023-12-04 12:05:30 +0100

    Optimize the process of retrieving search results

--- backend/src/search/models.py
+++ backend/src/search/models.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel
-from src.common.models import SearchTaskStatus
+from pydantic import BaseModel, ConfigDict, Field
 from src.api_parsers.models import Source
+from src.common.models import SearchTaskStatus
 
 
 class HistoryEntity(BaseModel):
@@ -14,25 +14,31 @@ HistoryResponseModel = list[HistoryEntity]
 
 
 class PublicationResponseModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     doi: str | None
     title: str
     year: int
     venues: list[str] | None
     abstract: str | None
-    citationCount: int | None
-    similarityScore: float | None
+    citation_count: int | None = Field(..., serialization_alias="citationCount")
+    similarity_score: float | None = Field(..., serialization_alias="similarityScore")
 
 
 class AuthorResponseModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: int
-    firstName: str
-    lastName: str
+    first_name: str = Field(..., serialization_alias="firstName")
+    last_name: str = Field(..., serialization_alias="lastName")
     email: str | None
     source: Source
     publication: PublicationResponseModel
 
 
 class SuggestionsResponseModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     authors: list[AuthorResponseModel]
     venues: set[str] | None
 
